15b57f5ee0e90e7300d74ea07c267d4fb2be1bc9
jingrui <jingrui192@gmail.com>
2014-02-09 17:19:52 -0800

    Create singleNumber2.py

---
+++ singleNumber2.py
@@ -0,0 +1,15 @@
+class Solution:
+    # @param A, a list of integer
+    # @return an integer
+    def singleNumber(self, A):
+        ones = A[0]
+        twos = 0
+        for ele in A[1:]:
+            twos |= ones&ele
+            ones ^= ele
+            three = ones^twos
+            ones &= ~three
+            twos &= ~three
+        return ones
+            
+            
