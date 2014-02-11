86949418bd88655457f0d61cd26e757712604610
jingrui <jingrui192@gmail.com>
2014-02-11 15:01:01 -0800

    Update BestTimetoBuyandSellStockIII.py

--- BestTimetoBuyandSellStockIII.py
+++ BestTimetoBuyandSellStockIII.py
@@ -1,6 +1,7 @@
 # BestTimetoBuyandSellStockIII
 # the idea is to find what can be earn from day 0 to day i and then from day i to day n-1
 # from day i to day n-1, we need to reverse the prices so that we can know that in O(n)
+# BestTimetoBuyandSellStockIII
 class Solution:
     # @param prices, a list of integer
     # @return an integer
@@ -9,25 +10,38 @@ class Solution:
 
         # from 0 to i
         low = prices[0]
+        maxp = 0
         profits1 = [0]
         for price in prices[1:]:
-            profits1.append(price-low)
-            if price < low:
-                low = price
-                
+        	p = price-low
+        	if p > maxp:
+        		maxp = p
+
+        	profits1.append(maxp)
+        	if price < low:
+        		low = price        
+        # print prices
+        # print profits1,'\n'
+
+
         # from i to end  
         prices = prices[::-1]
-        # print prices
         high = prices[0]
         profits2 = [0]
+        maxp = 0
         for price in prices[1:]:
-            profits2.append(high-price)
-            if price > high:
-                high = price
-
+        	p = high-price
+        	if p > maxp:
+        		maxp = p
+        	profits2.append(maxp)
+        	if price > high:
+        		high = price
+        # print profits2
+        prices = prices[::-1]
         profits2 = profits2[::-1]
+        # print profits2
         profits = []
         for ele in zip(profits1,profits2):
         	profits.append(ele[0]+ele[1])
-
-       	return max(profits)
+        # print profits
+        return max(profits) if max(profits)>0 else 0
